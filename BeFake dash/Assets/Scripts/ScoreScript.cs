331efc9b621592feffabc51abe7e646b2ed8a583
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-02-25 21:47:06 +0100

    cointext

--- BeFake dash/Assets/Scripts/ScoreScript.cs
+++ BeFake dash/Assets/Scripts/ScoreScript.cs
@@ -1,27 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
 using UnityEngine.UI;
 
 public class ScoreScript : MonoBehaviour
 {
 
-    //public Text MyscoreText;
-    //private int ScoreNum;
+    public Text MyscoreText;
+    private int ScoreNum = 0;
     void Start()
     {
-        //ScoreNum = 0;
-        //MyscoreText.text = "Score : " + ScoreNum;
+
+    }
+    void Update()
+    {
+        
     }
 
     private void OnTriggerEnter2D(Collider2D coin)
     {
         if(coin.tag == "MyCoin")
         {
-            //ScoreNum= 0;
+            ScoreNum+= 1;
             Destroy(coin.gameObject);
-        }
-        
+            MyscoreText.text = "" + ScoreNum;
+        } 
     }
 
 }
